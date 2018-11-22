d575e7a7c120da37309adbcccace68804cf22724
keigo.magami <keigo.magami@webcrew.co.jp>
2018-11-22 11:40:30 +0900

    Boxへの新規ファイル登録、既存ファイルの更新を検証

--- src/main/java/box/QuickStart.java
+++ src/main/java/box/QuickStart.java
@@ -1,7 +1,9 @@
 package box;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -9,25 +11,54 @@ import java.nio.file.Path;
 import java.util.stream.Stream;
 
 import com.box.sdk.BoxAPIConnection;
+import com.box.sdk.BoxConfig;
+import com.box.sdk.BoxDeveloperEditionAPIConnection;
 import com.box.sdk.BoxFolder;
 import com.box.sdk.BoxItem;
+import com.box.sdk.IAccessTokenCache;
+import com.box.sdk.InMemoryLRUAccessTokenCache;
 
 public class QuickStart {
 	public static void main(String args[]) throws IOException {
-		BoxAPIConnection api = new BoxAPIConnection("1To4qhpi3CBuSQe3S9B712cppT1Ty6ZN");
+		BoxAPIConnection api = new BoxAPIConnection("fgOIsgkVIzybuRFd9rm8sfJ2loofETNY");
 		BoxFolder rootFolder = BoxFolder.getRootFolder(api);
 		
 		for (BoxItem.Info itemInfo : rootFolder) {
 		    System.out.format("[%s] %s\n", itemInfo.getID(), itemInfo.getName());
 		}
+//		
+//		// ファイルの読み込みまで検証
+//		Path path = FileSystems.getDefault().getPath("resource", "sample.txt");
+//		BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+//		Stream<String> lines = br.lines();
+//		lines.forEach(line -> {System.out.println(line);});
+//		br.close();
 		
-		// ファイルの読み込みまで検証
-		Path path = FileSystems.getDefault().getPath("resource", "sample.txt");
-		BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8);
-		Stream<String> lines = br.lines();
-		lines.forEach(line -> {System.out.println(line);});
-		br.close();
 		
+		 //It is a best practice to use an access token cache to prevent unneeded requests to Box for access tokens.
+        //For production applications it is recommended to use a distributed cache like Memcached or Redis, and to
+        //implement IAccessTokenCache to store and retrieve access tokens appropriately for your environment.
+        
+		// アクセストークンのキャッシュ設定（多分使い終わったらdeleteするのが吉）
+//		IAccessTokenCache accessTokenCache = new InMemoryLRUAccessTokenCache(1);
+//
+//        Reader reader = new FileReader("resource/config.json");
+//        BoxConfig boxConfig = BoxConfig.readFrom(reader);
+//        System.out.println(boxConfig.getClientId());
+//        System.out.println(boxConfig.getClientSecret());
+//        System.out.println(boxConfig.getEnterpriseId());
+//        System.out.println(boxConfig.getJWTEncryptionPreferences());
+//        
+//        BoxDeveloperEditionAPIConnection api = BoxDeveloperEditionAPIConnection.getAppEnterpriseConnection(
+//                boxConfig, accessTokenCache);
+//        
+//		BoxFolder rootFolder = BoxFolder.getRootFolder(api);
+//		
+//		for (BoxItem.Info itemInfo : rootFolder) {
+//		    System.out.format("[%s] %s\n", itemInfo.getID(), itemInfo.getName());
+//		}
+//		
+//		api.revokeToken();
 		
 	}
 }
---
+++ src/main/java/box/UploadFile.java
@@ -0,0 +1,79 @@
+package box;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import com.box.sdk.BoxAPIConnection;
+import com.box.sdk.BoxConfig;
+import com.box.sdk.BoxDeveloperEditionAPIConnection;
+import com.box.sdk.BoxFile;
+import com.box.sdk.BoxFolder;
+import com.box.sdk.BoxItem;
+import com.box.sdk.BoxItem.Info;
+import com.box.sdk.IAccessTokenCache;
+import com.box.sdk.InMemoryLRUAccessTokenCache;
+
+public class UploadFile {
+	public static void main(String args[]) throws IOException{
+		
+		BoxAPIConnection api = new BoxAPIConnection("a61ihOtLIhPRyH8SD2U6wDyFNBgAQjh8");
+		BoxFolder rootFolder = BoxFolder.getRootFolder(api);
+		
+		for (BoxItem.Info itemInfo : rootFolder) {
+			System.out.format("[%s] %s\n", itemInfo.getID(), itemInfo.getName());
+		}
+		
+		
+		// Box内のフォルダの指定
+		BoxFolder localFolder = new BoxFolder(api, "58828304698");
+		for(BoxItem.Info itemInfo : localFolder) {
+			System.out.format("[%s] %s\n", itemInfo.getID(), itemInfo.getName());
+		}
+		
+		// Box内の対象フォルダにファイルが存在するかを判定
+		String existFileId = isExistFile("sample.txt", localFolder);
+		System.out.println(existFileId);
+		
+		File file =	new File("resource/sample.txt");
+		
+		try(FileInputStream fileInputStream = new FileInputStream(file)){
+			
+			// Boxに対象フォルダが存在するならば更新して、なければ新規登録する
+			if(existFileId.isEmpty()) {
+				BoxFile.Info newFileInfo = localFolder.uploadFile(fileInputStream, "sample.txt");
+			}else {
+				BoxFile boxFile = new BoxFile(api, existFileId);
+				boxFile.uploadNewVersion(fileInputStream);
+				
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+//			api.revokeToken();
+		}
+		
+		
+	}
+	
+	
+	private static String isExistFile(String fileName, BoxFolder targetFolder) {
+		
+		Stream<Info> boxFileStream = StreamSupport.stream(targetFolder.spliterator(), false);
+		return boxFileStream.filter(file -> file.getName().equals(fileName)).map(Info :: getID).findFirst().orElse("");
+		
+	}
+	
+
+
+}
