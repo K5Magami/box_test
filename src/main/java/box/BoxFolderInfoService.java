a909dfdf10bcba4d87998bba6e26944f31e5a409
keigo_MGM <k5.magami@gmail.com>
2019-01-07 09:41:55 +0900

    Remote work (#3)
    * Dockerfile修正
    
    * API Connection取得用に別クラスを作成

--- src/main/java/box/BoxFolderInfoService.java
+++ src/main/java/box/BoxFolderInfoService.java
@@ -14,46 +14,40 @@ import com.box.sdk.BoxItem.Info;
 import api.box.model.response.BoxFolderInfo;
 
 public class BoxFolderInfoService {
-	public BoxFolderInfo execute(long targetFolderId, String targetFolderName) {
-		
+	public BoxFolderInfo execute(long targetFolderId, String targetFolderName) throws Exception {
 		
 		// アクセストークンのキャッシュ設定（多分使い終わったらdeleteするのが吉）
 		IAccessTokenCache accessTokenCache = new InMemoryLRUAccessTokenCache(10);
 		
-		Reader reader = null;
-		BoxConfig boxConfig = null;
+		BoxDeveloperEditionAPIConnection api = null;
+		String searchResult = "";
 		
+		// TODO apiのオブジェクトに関してはDIするように設定したい
 		try {
-			reader = new FileReader("resource/config.json");
-	        boxConfig = BoxConfig.readFrom(reader);
+			BoxApiConnectionService boxApiConnectionService = new BoxApiConnectionService();
+	        api = boxApiConnectionService.getConnection();
+	        api.asUser("3560300031");
+	        
+	        // 検索元のFolder情報を取得
+	        BoxFolder folder = new BoxFolder(api, String.valueOf(targetFolderId));
+	        
+	        // 指定したフォルダー名と一致するフォルダーを取得する
+	        searchResult = StreamSupport.stream(folder.spliterator(), false)
+	        		.filter(info -> info.getName().equals(targetFolderName) && info.getType().equals("folder")).map(Info::getID)
+	        		.findFirst().orElse("");
 		} catch (Exception e) {
-			// TODO: handle exception
-			
+			throw e;
+		} finally {
+			if(api != null) {
+				api.revokeToken();
+			}
 		}
-
-        System.out.println(boxConfig.getClientId());
-        System.out.println(boxConfig.getClientSecret());
-        System.out.println(boxConfig.getEnterpriseId());
-        System.out.println(boxConfig.getJWTEncryptionPreferences());
-
-        BoxDeveloperEditionAPIConnection api = BoxDeveloperEditionAPIConnection.getAppEnterpriseConnection(
-                boxConfig, accessTokenCache);
-        api.asUser("3560300031");
-
-		// 検索元のFolder情報を取得
-		BoxFolder folder = new BoxFolder(api, String.valueOf(targetFolderId));
-
-		// 指定したフォルダー名と一致するフォルダーを取得する
-		String searchResult = StreamSupport.stream(folder.spliterator(), false)
-				.filter(info -> info.getName().equals(targetFolderName) && info.getType().equals("folder")).map(Info::getID)
-				.findFirst().orElse("");
 
 		// Boxフォルダーの有無
 		boolean isExists = !searchResult.isEmpty();
 		// BoxフォルダーのIDをint型に変換する.(存在しない場合は-1)
 		long folderId = isExists ? Long.valueOf(searchResult) : -1;
 
-		api.revokeToken();
 		return new BoxFolderInfo(isExists, folderId);
 	}
 
